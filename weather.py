48b2570a0e20e0ce93959d39e9a49b518830c2d8
sk1z11 <senyaz1995@gmail.com>
2023-05-02 00:54:59 +0500

    weather-bot-v1.1

--- weather.py
+++ weather.py
@@ -1,6 +1,7 @@
 import telebot
 import requests
 import json
+import datetime
 
 bot = telebot.TeleBot('5787802812:AAGfBlJTVGECXwOtnk8A1jfhHcE9HBszWAM')
 API = 'af764beb4f6b7845ea428759076d7711'
@@ -18,12 +19,14 @@ def get_weather(message):
     if res.status_code == 200:
         data = json.loads(res.text)
         temp = int(data["main"]["temp"])
-        bot.reply_to(message, f'Температура на улице: {temp} °C')
-
+        temp_fl = int(data["main"]["feels_like"])
+        wind_sp = int(data["wind"]["speed"])
+        cur_data = str(datetime.datetime.fromtimestamp(data["dt"]))
+        bot.send_message(message.chat.id, f'{cur_data}\nТемпература на улице: {temp} °C\nОщущается как: {temp_fl}°C\nСкорость ветра: {wind_sp} м/c')
         emoji = '☀' if temp > 5.0 else '⛅'
         bot.send_message(message.chat.id, emoji)
     else:
-        bot.reply_to(message, f'Такого города не существует')
+        bot.send_message(message.chat.id, f'Такого города не существует')
 
 
 bot.polling(none_stop=True)
